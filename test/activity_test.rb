28dfe8d5839131910ad41d6539a70d22bea2718f
brickstar <brickstar@gmail.com>
2018-04-05 16:35:04 -0600

    initial commit

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,31 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @act = Activity.new("Golf")
+  end
+
+  def test_it_exists
+    assert_instance_of Activity, @act
+  end
+
+  def test_it_has_a_name
+    assert_equal "Golf", @act.name
+  end
+
+  def test_participants_starts_as_empty_hash
+
+  end
+
+  def test_it_can_add_participants
+
+  end
+
+  def test_participant_attributes
+
+  end
+
+  def test_it_can_evaluate_costs
+
+  end
 end
