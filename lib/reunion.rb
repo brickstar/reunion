8a844acbc2e023dc185204d7f2366051a0dab30d
brickstar <brickstar@gmail.com>
2018-04-05 17:23:09 -0600

    activities and location

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,7 @@
 class Reunion
+  attr_reader :location, :activities
+  def initialize(location)
+    @location = location
+    @activities = Hash.new(0)
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -5,8 +5,27 @@ require './lib/reunion'
 
 class ReunionTest < Minitest::Test
   def setup
-    @r = Reunion.new
+    @r = Reunion.new("Denver")
     @act_1 = Activity.new("Golf")
     @act_2 = Activity.new("Tennis")
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, @r
+  end
+
+  def test_it_has_a_location
+    assert_equal "Denver", @r.location
+  end
+
+  def test_it_has_activities_and_starts_empty
+    expected = Hash.new(0)
+    actual = @r.activities
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_add_activities
+
   end
 end
